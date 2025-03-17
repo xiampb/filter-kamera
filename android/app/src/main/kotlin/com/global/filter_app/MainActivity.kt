d110284c97773bbe85df941b5cef766d3e68650b
xiampb <ilhamadityapb@gmail.com>
2025-03-17 18:26:28 +0700

    Add Flutter Filter Kamera project files

---
+++ android/app/src/main/kotlin/com/global/filter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.global.filter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widget/filter_carousel.dart
@@ -0,0 +1,68 @@
+// lib/widget/filter_carousel.dart
+import 'package:filter_app/widget/filter_selector.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class PhotoFilterCarousel extends StatefulWidget {
+  const PhotoFilterCarousel({super.key});
+
+  @override
+  State<PhotoFilterCarousel> createState() => _PhotoFilterCarouselState();
+}
+
+class _PhotoFilterCarouselState extends State<PhotoFilterCarousel> {
+  final _filters = [
+    Colors.white,
+    ...List.generate(
+      Colors.primaries.length,
+      (index) => Colors.primaries[(index * 4) % Colors.primaries.length],
+    )
+  ];
+
+  final _filterColor = ValueNotifier<Color>(Colors.white);
+
+  void _onFilterChanged(Color value) {
+    _filterColor.value = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _buildPhotoWithFilter(),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: _buildFilterSelector(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoWithFilter() {
+    return ValueListenableBuilder(
+      valueListenable: _filterColor,
+      builder: (context, color, child) {
+        return Image.network(
+          'https://docs.flutter.dev/cookbook/img-files/effects/instagram-buttons/millennial-dude.jpg',
+          color: color.withOpacity(0.5),
+          colorBlendMode: BlendMode.color,
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterSelector() {
+    return FilterSelector(
+      onFilterChanged: _onFilterChanged,
+      filters: _filters,
+    );
+  }
+}
